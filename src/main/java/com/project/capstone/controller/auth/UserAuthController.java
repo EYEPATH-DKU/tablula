8f7f134c7e63034d23161d3c8393defb5dcbea7e
HaByeong <121917642+HaByeong@users.noreply.github.com>
2025-06-01 23:08:07 +0900

    feat: JWT AccessToken 재발급 API 구현

--- src/main/java/com/project/capstone/controller/auth/UserAuthController.java
+++ src/main/java/com/project/capstone/controller/auth/UserAuthController.java
@@ -1,18 +1,24 @@
 package com.project.capstone.controller.auth;
 
+import com.project.capstone.dto.auth.ReissuedTokenDto;
 import com.project.capstone.dto.auth.UserLoginRequestDto;
 import com.project.capstone.dto.auth.UserLoginResponseDto;
+import com.project.capstone.service.auth.AccessTokenReissueService;
 import com.project.capstone.service.auth.UserLogOutServiceInterface;
 import com.project.capstone.service.auth.UserLoginServiceInterface;
 import com.project.capstone.service.signup.UserSignUpServiceInterface;
 import com.project.capstone.service.user.UserUpdateServiceInterface;
 import com.project.capstone.service.userinfo.UserInfoServiceInterface;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @RestController // HTTP 요청을 처리하는 컨트롤러를 의미
@@ -21,6 +27,7 @@ public class UserAuthController {
 
     private final UserLoginServiceInterface loginService;
     private final UserLogOutServiceInterface userLogOutService;
+    private final AccessTokenReissueService accessTokenReissueService;
 
     @PostMapping("/login")
     public ResponseEntity userLogIn(@RequestBody UserLoginRequestDto userLoginRequestDto) {
@@ -34,7 +41,23 @@ public class UserAuthController {
 
     @PostMapping("/logout")
     public void userLogOut(){
+
         userLogOutService.logout();
     };
+
+    //Access 토큰 만료시 프론트에서 /reissue로 넘겨줘야함
+    @PostMapping("/reissue")
+    public ReissuedTokenDto reissueAccessToken(HttpServletRequest request) {
+        String refreshToken = Arrays.stream(request.getCookies())
+                .filter(c -> c.getName().equals("refreshToken"))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(() -> new IllegalArgumentException("쿠키에 Refresh Token이 없습니다."));
+
+        return new ReissuedTokenDto(
+                accessTokenReissueService
+                        .reissueAccessToken(refreshToken)
+        );
+    }
 
 }
